b971dfcf2e12aa58aa533141b84493ef365fe13c
ahmedanjumfahad <f20_777@yahoo.com>
2015-01-16 16:25:17 +0600

    calculator

---
+++ calculator.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Calculator</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+</div>&nbsp;</div>
+
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading"><center><h3>Calculator</h3></center></div>
+            <div class="panel-body">
+                <form class="form-horizontal" role="form" method="post">
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Number1</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="inputEmail3" placeholder="number">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Number2</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="inputPassword3" placeholder="number">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="calculator"> Add
+                                </label>
+                                <label>
+                                    <input type="radio" name="calculator"> Subtraction
+                                </label>
+                                <label>
+                                    <input type="radio" name="calculator"> Multiplication
+                                </label>
+                                <label>
+                                    <input type="radio" name="calculator"> Devition
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default"><b>Calculate</b></button>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
